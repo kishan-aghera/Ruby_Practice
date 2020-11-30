3cc3e3b94fd0badbab83f60a175c638644be3eeb
Kishan Aghera <kishan-aghera@users.noreply.github.com>
2020-11-30 12:04:23 +0530

    Added comments to both the solutions.

--- Exercise/Exercise1.rb
+++ Exercise/Exercise1.rb
@@ -1,20 +1,20 @@
 p "Guess a number"
-n = gets
-r = rand 1..30
-p r
+n = gets # program will ask user to guess the number
+r = rand 1..30 # program will guess any number from 1 to 30
+# p r
 attempts = 0
-while attempts < 6
-  if n == r
+while attempts < 6 # max attempts are 5
+  if n == r # if user perfectly guess the number, we have to print "congrats you won!"
     puts "Congrats, You Won!"
     break
   end
-  if (n - r).abs <= 5
+  if (n - r).abs <= 5 # if number is 5 steps nearer to the guessed number we need to print "You are too close!"
     puts "You are too close!"
-  # else
-  #   puts "You are far"
+  else # if not print "please try again!"
+    puts "Please try again!"
   end
   attempts = attempts + 1
 end
-if attempts == 6
+if attempts == 6 # if 5 attempts are over then we have to print "You lose!"
   puts "You lose"
 end
--- Exercise/Exercise2.rb
+++ Exercise/Exercise2.rb
@@ -1,16 +1,16 @@
-def length_of_string(str)
+def length_of_string(str) # Length
   str.length
 end
 
-def get_first_character(str)
+def get_first_character(str) # First character
   str[0]
 end
 
-def get_last_character(str)
+def get_last_character(str) # Last character
   str[-1]
 end
 
-def get_middle_character(str)
+def get_middle_character(str) # Middle character, if the string has an odd number of characters. Middle TWO characters, if the string has an even number of characters.
   if str.length.even?
       return str[str.length/2-1] + str[str.length/2]
   else
@@ -18,7 +18,7 @@ def get_middle_character(str)
   end
 end
 
-def get_second_occurrence(str, c)
+def get_second_occurrence(str, c) #  Index of the second occurrence of the second character
   occur = 0
   i = 0
   while i < str.length do
@@ -34,7 +34,7 @@ def get_second_occurrence(str, c)
 end
 
 p "Enter a string"
-# s = gets Have to figure out this line.
+# s = gets # Have to figure out this line
 p length_of_string(s)
 p get_first_character(s)
 p get_last_character(s)
